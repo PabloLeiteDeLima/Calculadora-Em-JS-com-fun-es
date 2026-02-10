34e03202f85d6572b97781b06ce021836177659f
Pablo Leite <pabloleitelima7@gmail.com>
2026-02-10 03:35:56 -0300

    identando todo o código

--- calculadoraFuncoes.js
+++ calculadoraFuncoes.js
@@ -5,43 +5,41 @@
 
 const readline = require('readline')
 const teclado = readline.createInterface({
-    input:  process.stdin,
+    input: process.stdin,
     output: process.stdout
 })
 
 teclado.question('Informe seu nome: ', (nome) => {
+    teclado.question('Digite o primeiro valor: ', (valor1) => {
+        teclado.question('Qual operação quer fazer (+, -, *, /): ', (op) => {
+            teclado.question('Digite o segundo valor: ', (valor2) => {
 
+                // Funções para executar as operações (arrow function)
+                const soma = (v1, v2) => v1 + v2
+                const subtracao = (v1, v2) => v1 - v2
+                const multiplicacao = (v1, v2) => v1 * v2
+                const divisao = (v1, v2) => v1 / v2
 
-teclado.question('Digite o primeiro valor: ', (valor1) =>{
-    teclado.question('Qual operação quer fazer (+, -, *, /): ', (op) => {    
-        teclado.question('Digite o segundo valor: ', (valor2) => {
-        
-        // Funções para executar as operações (arrow function)
-        const soma = (v1, v2) => v1 + v2
-        const subtracao = (v1, v2) => v1 - v2
-        const multiplicacao = (v1, v2) => v1 * v2
-        const divisao = (v1, v2) => v1 / v2
-        
-        // Se liga que a interface retorna tudo em string então vamos transformar em inteiro.
-        const v1 = parseInt(valor1)
-        const v2 = parseInt(valor2)
+                // Se liga que a interface retorna tudo em string então vamos transformar em inteiro.
+                const v1 = parseInt(valor1)
+                const v2 = parseInt(valor2)
 
-        console.log('\n',nome,',Voçe escolheu a operação: (',op,')')    
-        // Aqui vamos tratar a operação escolhida pelo usuário.
-        if(op == '+'){
-            console.log(v1, ' + ', v2, ' = ', v1 + v2)    
-        }else if(op == '-'){
-            console.log(v1, ' - ', v2, ' = ', v1 - v2)
-        }else if(op == '*'){
-            console.log(v1, ' * ', v2, ' = ', v1 * v2)
-        }else if(op == '/'){
-            console.log(v1, ' / ', v2, ' = ', v1 / v2)
-        }else{// Tratando quando usuário escolhe a operação diferente/inválida.
-            console.log('Escolha uma operação válida.')
-            console.log('Operações válidas para escolha (+, -, *, /)')
-        }
-        teclado.close()// Fechando a interface
-        })    
+                console.log('\n', nome, ',Voçe escolheu a operação: (', op, ')')
+                // Aqui vamos tratar a operação escolhida pelo usuário.
+                if (op == '+') {
+                    console.log(v1, ' + ', v2, ' = ', v1 + v2)
+                } else if (op == '-') {
+                    console.log(v1, ' - ', v2, ' = ', v1 - v2)
+                } else if (op == '*') {
+                    console.log(v1, ' * ', v2, ' = ', v1 * v2)
+                } else if (op == '/') {
+                    console.log(v1, ' / ', v2, ' = ', v1 / v2)
+                } else {// Tratando quando usuário escolhe a operação diferente/inválida.
+                    console.log('Escolha uma operação válida.')
+                    console.log('Operações válidas para escolha (+, -, *, /)')
+                }
+                teclado.close()// Fechando a interface
+            })
+        })
     })
-})
 })
