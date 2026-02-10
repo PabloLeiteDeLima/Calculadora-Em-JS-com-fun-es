8373514f0234ef2e3d098a6ee783fb36ac61e330
Pablo Leite <pabloleitelima7@gmail.com>
2026-02-10 03:13:05 -0300

    primeira atualização

---
+++ calculadoraFuncoes.js
@@ -0,0 +1,46 @@
+// Fiz uma calculadora.
+// Pegando os valores e a operação digitada pelo usuário, via teclado.
+// Mostrando para o usuário o resultado.
+// 09/02/2016. Pablo Leite de Lima.
+
+const readline = require('readline')
+const teclado = readline.createInterface({
+    input:  process.stdin,
+    output: process.stdout
+})
+
+teclado.question('Digite o primeiro valor: ', (valor1) =>{
+    teclado.question('Qual operação quer fazer (+, -, *, /): ', (op) => {    
+        teclado.question('Digite o segundo valor: ', (valor2) => {
+        
+        // Funções para executar as operações (arrow function)
+        const soma = (v1, v2) => v1 + v2
+        const subtracao = (v1, v2) => v1 - v2
+        const multiplicacao = (v1, v2) => v1 * v2
+        const divisao = (v1, v2) => v1 / v2
+        
+        // Se liga que a interface retorna tudo em string então vamos transformar em inteiro.
+        const v1 = parseInt(valor1)
+        const v2 = parseInt(valor2)
+
+        // Aqui vamos tratar a operação escolhida pelo usuário.
+        if(op == '+'){
+            console.log('\n\nVoçe escolheu a soma.')
+            console.log(v1, ' + ', v2, ' = ', v1 + v2)    
+        }else if(op == '-'){
+            console.log('\n\nVoçe escolheu a subtração.')
+            console.log(v1, ' - ', v2, ' = ', v1 - v2)
+        }else if(op == '*'){
+            console.log('\n\nVoçe escolheu a multiplicação.')
+            console.log(v1, ' * ', v2, ' = ', v1 * v2)
+        }else if(op == '/'){
+            console.log('\n\nVoçe escolheu a multiplicação.')
+            console.log(v1, ' / ', v2, ' = ', v1 / v2)
+        }else{// Tratando quando usuário escolhe a operação diferente.
+            console.log('Escolha uma operação válida.')
+            console.log('Operações válidas para escolha (+, -, *, /)')
+        }
+        teclado.close()// Fechando a interface
+        })    
+    })
+})
